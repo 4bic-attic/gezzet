6bef2ff87c5aa54943d3b9502e4b9c93a873c08c
4bic <robyne.kiplangat@gmail.com>
2016-02-23 12:57:35 +0300

    crawl & extract links

--- gezzet/items.py
+++ gezzet/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class GezzetItem(scrapy.Item):
+class RwItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    filename = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
