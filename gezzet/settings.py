f8eb50c507024fba66534f0470a8afc99e09a625
4bic <robyne.kiplangat@gmail.com>
2016-02-23 13:46:18 +0300

    activate pipeline

--- gezzet/settings.py
+++ gezzet/settings.py
@@ -61,9 +61,9 @@ NEWSPIDER_MODULE = 'gezzet.spiders'
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'gezzet.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'gezzet.pipelines.GezzetPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
---
+++ items.jl
@@ -0,0 +1,28 @@
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": [".."]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2004 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2005 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2006 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2007 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2008 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2009 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2010 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2011 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2012 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2013 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2014 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2015 Official Gazettes"]}
+{"filename": [], "link": [], "desc": []}
+{"filename": [], "link": [], "desc": ["2016_Official_Gazettes"]}
