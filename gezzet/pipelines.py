e3ac61097b81f72742bc9fbdcb43b41692d2a7ca
4bic <robyne.kiplangat@gmail.com>
2016-02-23 13:40:59 +0300

    save scraped data in json lines

--- gezzet/pipelines.py
+++ gezzet/pipelines.py
@@ -4,8 +4,16 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import json
 
 class GezzetPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+    def __init__(self):
+        self.file = open('items.jl', 'wb') #use jsonlines for better scale purposes
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
